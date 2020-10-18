a2ff3e9c844eeb43a00e4d80151e9810c5a7bbe5
666q <gustavo.carvalho@aluno.faculdadeimpacta.com.br>
2020-10-17 23:36:05 -0300

    AC

--- ac.py
+++ ac.py
@@ -1,26 +1,29 @@
 import os
-from flask import Flask, jsonify, request
-from math import sqrt
+from flask import Flask
 
-app = Flask(name)
+app = Flask(__name__)
+
 @app.route('/')
-def primos():
-    c = 1
-    p = 1
-    numero = 3
-    primos = "2,"
 
-    while p < 100:
-        eprimo = 1
-        for i in range(2, numero):
-            if numero % i == 0:
-                eprimo = 0
+def nao_entre_em_panico():
+    limit = 100
+    primos_showed = 2
+    n = 4
+    primos = "2, 3, "
+    while primos_showed < limit:
+        eh_primo = True
+        for i in range(2, n):
+            if n % i == 0:
+                eh_primo = False
                 break
-        if (eprimo):
-            primos = primos + str(numero) + ","
-            p += 1
-        numero += 1
+        if eh_primo:
+            primos += f"{n}, " if (primos_showed < limit - 1) else f"{n}."
+            primos_showed += 1
+        n += 1
     return primos
-if name == "main":
+
+
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
